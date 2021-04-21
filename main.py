2090613ba4b52558ae522421cd389866ca056d52
dan <popescudand@gmail.com>
2021-04-21 07:14:59 +0300

    Fix the main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ddpg import DDPG
 
-ddpg = DDPG(seed=17)
+ddpg = DDPG(seed=1)
 
-result, final_eval_score, training_time, wallclock_time = ddpg.train(episodes=50_000)
+ddpg.train(episodes=50_000)
